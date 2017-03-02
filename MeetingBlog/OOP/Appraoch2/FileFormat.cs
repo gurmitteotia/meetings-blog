0e4fdcd1c6af10be7cb71a40772d9668d20abebe
Gurmit Singh <gurmit.singh@cloudhouse.com>
2017-03-02 14:48:30 +0000

    Add another approach in OOP style

---
+++ MeetingBlog/OOP/Appraoch2/FileFormat.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MeetingBlog.OOP.Appraoch2
+{
+    public class FileFormat
+    {
+        private readonly Func<StreamReader, IEnumerable<Meeting>> _meetingParser;
+        private FileFormat(Func<StreamReader, IEnumerable<Meeting>> meetingParser)
+        {
+            _meetingParser = meetingParser;
+        }
+        public static readonly FileFormat Csv = new FileFormat(ParseFromCsvFormat);
+        public static readonly FileFormat Xml = new FileFormat(ParseFromXmlFormat);
+
+        public IEnumerable<Meeting> ParseFrom(StreamReader fileReader)
+        {
+            return _meetingParser(fileReader);
+        }
+        private static IEnumerable<Meeting> ParseFromCsvFormat(StreamReader dataReader)
+        {
+            var meetingLines = dataReader.ReadAllLines().Skip(1).Select(line => new MeetingLine(line));
+            return meetingLines.Select(ml => ml.Meeting).ToArray();
+        }
+
+        private static IEnumerable<Meeting> ParseFromXmlFormat(StreamReader dataReader)
+        {
+            //TODO: implement xml parsing
+            throw new NotImplementedException();
+        }
+        private class MeetingLine
+        {
+            private readonly string _line;
+            public MeetingLine(string line)
+            {
+                _line = line;
+            }
+            public Meeting Meeting
+            {
+                get
+                {
+                    var meeting = _line.Split(',');
+                    return new Meeting(meeting[0], meeting[1], ParseDate(meeting[2]), ParseDate(meeting[3]), ParseDate(meeting[4]));
+                }
+            }
+            private DateTime ParseDate(string date)
+            {
+                DateTime parsedDate;
+                if (DateTime.TryParse(date, out parsedDate))
+                    return parsedDate;
+
+                throw new BadDateException(string.Format("Can not parse date {0} from line {1}", date, _line));
+            }
+        }
+    }
+}
--- MeetingBlog/OOP/MeetingFile.cs
+++ MeetingBlog/OOP/MeetingFile.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Castle.Core.Internal;
 
 
@@ -17,7 +15,7 @@ namespace MeetingBlog.OOP
         public static MeetingFile Csv(string path)
         {
             var meetingLines = File.ReadAllLines(path).Skip(1).Select(line => new MeetingLine(line));
-            return new MeetingFile(meetingLines.Select(ml=>ml.Meeting).ToArray());
+            return new MeetingFile(meetingLines.Select(ml => ml.Meeting).ToArray());
         }
 
         public void ScheduleIn(Calendar calendar)
@@ -43,10 +41,10 @@ namespace MeetingBlog.OOP
             private DateTime ParseDate(string date)
             {
                 DateTime parsedDate;
-                if(DateTime.TryParse(date, out parsedDate))
+                if (DateTime.TryParse(date, out parsedDate))
                     return parsedDate;
 
-                throw new BadDateException(string.Format("Can not parse date {0} from line {1}",date,_line));
+                throw new BadDateException(string.Format("Can not parse date {0} from line {1}", date, _line));
             }
         }
     }
